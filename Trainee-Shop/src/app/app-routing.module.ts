0e15d48fd58894c1a284e75fc26c18e7e66b2ae6
cmaldonado98 <carlos.maldonado1@cloud.uautonoma.cl>
2021-09-07 20:51:46 -0500

    Eliminar componentes no usados, add register

--- Trainee-Shop/src/app/app-routing.module.ts
+++ Trainee-Shop/src/app/app-routing.module.ts
@@ -1,5 +1,3 @@
-import { EditarComponent } from './vistas/editar/editar.component';
-import { NuevoComponent } from './vistas/nuevo/nuevo.component';
 import { DashboardComponent } from './vistas/dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,9 +6,7 @@ import {LoginComponent} from './vistas/login/login.component';
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
   {path: 'login', component:LoginComponent},
-  {path: 'dashboard', component:DashboardComponent},
-  {path: 'nuevo', component:NuevoComponent},
-  {path: 'editar', component:EditarComponent}
+  {path: 'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
@@ -18,4 +14,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, DashboardComponent, NuevoComponent, EditarComponent]
+export const routingComponents = [LoginComponent, DashboardComponent]
