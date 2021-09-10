07dce980a6b3c18949e0031b45091cd682c34b14
Carlos Maldonado <carlos.maldonado1@cloud.uautonoma.cl>
2021-09-09 22:26:50 -0500

    Add guards, login and logout, header and footer

--- Trainee-Shop/src/app/app-routing.module.ts
+++ Trainee-Shop/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './vistas/login/login.component';
 import { RegistroComponent } from './vistas/registro/registro.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
   {path: 'login', component:LoginComponent},
-  {path: 'dashboard', component:DashboardComponent},
+  {path: 'dashboard', component:DashboardComponent,canActivate:[AuthGuard]},
   {path: 'registro', component:RegistroComponent}
 ];
 
