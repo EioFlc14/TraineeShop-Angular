bc2810fbe7f3af28bb335388a9e6c2ab98a17b71
GAZUMBA <gazumba@uce.edu.ec>
2021-09-10 03:13:42 -0500

    enviando el id del super en el boton de cada supermercado

--- Trainee-Shop/src/app/servicios/api/api-supermercado.service.ts
+++ Trainee-Shop/src/app/servicios/api/api-supermercado.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Supermercado } from '../modelos/supermercado';
+import { Supermercado } from '../../modelos/supermercado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiSupermercadoService {
+  //private idSupermecado$ = new Subject<any>();
 
   private baseEndpoint = "https://api-supermercados.herokuapp.com/";
   private cabeceras: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
   constructor(private http: HttpClient) { }
 
   public listarSupermercados(): Observable<Supermercado[]> {
-    //this.http.get<Supermercado[]>(this.baseEndpoint)
     return this.http.get(this.baseEndpoint).pipe(
       map(supermercados => {
         return supermercados as Supermercado[]
       })
     );
+    //this.idSupermecado$.next(Supermercado);
 
   }
+  
 }
--- Trainee-Shop/src/app/vistas/supermercados/supermercados.component.ts
+++ Trainee-Shop/src/app/vistas/supermercados/supermercados.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 import { Supermercado } from 'src/app/modelos/supermercado';
-import { ApiSupermercadoService } from 'src/app/servicios/api-supermercado.service';
+import { ApiSupermercadoService } from 'src/app/servicios/api/api-supermercado.service';
 
 @Component({
   selector: 'app-supermercados',
@@ -8,16 +10,36 @@ import { ApiSupermercadoService } from 'src/app/servicios/api-supermercado.servi
   styleUrls: ['./supermercados.component.css']
 })
 export class SupermercadosComponent implements OnInit {
+  // private idSupermecado$ = new Subject<any>();
   //subcription: Subscription;
   // usuario:string;
   //password:string;
   titulo = "SUPERMERCADOS";
-  supermercados: Supermercado[]= [];
-  constructor(private service: ApiSupermercadoService) { }
+  supermercados: Supermercado[] = [];
+  constructor(private service: ApiSupermercadoService, private router: Router) { }
 
   ngOnInit(): void {
-   let total= this.service.listarSupermercados().subscribe(supermercados => this.supermercados = supermercados);
-    console.log(total);
+    let total = this.service.listarSupermercados().subscribe(supermercados => this.supermercados = supermercados);
+    // this.idSupermecado$.next(this.supermercados);
   }
+  /*getID(): Observable<any> {
+    return this.idSupermecado$.asObservable();
+  }
+  */
+
+  enviarIdSuper(id: number) {
+    console.log(id);
+    console.log(id);
+    console.log(23424343);
+    console.log(23424343);
+    console.log(23424343);
+    console.log(23424343);
+    console.log(id);
+    console.log("estoy probando")
+  }
+
+
+
+
 
 }
