9b70a32b38c652496ba99c97a1b85163d7fc77c9
Edison <edison14_@hotmail.com>
2021-09-09 22:34:51 -0500

    Updated api registro

--- Trainee-Shop/src/app/servicios/api/api-registro.service.ts
+++ Trainee-Shop/src/app/servicios/api/api-registro.service.ts
@@ -10,25 +10,21 @@ import {Observable} from 'rxjs'
 })
 export class ApiRegistroServicio {
 
+
+  url:string = "http://34.227.98.168:4003/clientes/";
+
   constructor(
     private _http:HttpClient
     ) { }
 
   getByCedula(cedula:string):Observable<ResponsRegistroI>{
-    var url = "http://34.227.98.168:4003/clientes/login/";
-    let direccion = url + cedula;
-    return this._http.get<ResponsRegistroI>(direccion);
-  }
-
-  getall():Observable<any>{
-    var url = "http://34.227.98.168:4003/clientes/login/";
-    let direccion = url; // + cedula;
+    var login = "login/";
+    let direccion = this.url + login + cedula;
     return this._http.get<ResponsRegistroI>(direccion);
   }
 
   crearCuenta(registro:any):Observable<any>{
-    var url = "http://34.227.98.168:4003/clientes/";
-    let direccion = url;
+    let direccion = this.url;
     return this._http.post<any>(direccion,registro);
   }
 
