d64aeb019f6715fc5336558d5c26e7fc0a7bdc2c
AlexanderJr1996 <alexander_navarrete2011@hotmail.com>
2021-09-14 08:55:48 -0500

    2021Sep14

--- Trainee-Shop/src/app/vistas/producto/producto.component.ts
+++ Trainee-Shop/src/app/vistas/producto/producto.component.ts
@@ -1,23 +1,7 @@
-/*import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-producto',
-  templateUrl: './producto.component.html',
-  styleUrls: ['./producto.component.css']
-})
-export class ProductoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}*/
 import { Component } from '@angular/core';
 import { ProductoService } from '../../servicios/api/api-productos.service';
 import {MatDialogModule, MatDialogConfig, MatDialogRef, MatDialog} from '@angular/material/dialog'; 
 import { Router } from '@angular/router';
-//import { PagoComponent } from '../pago/pago.component';
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
@@ -34,51 +18,30 @@ export class ProductoComponent {
   res:number=0;
   sup_recibido:string="";
   constructor(
-    private taskService:ProductoService,private router: Router//,public dialog: MatDialog
-    //
+    private taskService:ProductoService,private router: Router
   )  {
     this.getAllTasks();
-    //const findSup = this.tasks.find((task: { Supermercado_idSupermercado: string; }) => task.Supermercado_idSupermercado == '3');
-   
-
-  //  console.log('Lista Supermercado id = 3',findSup);
   }
-
   getAllTasks(){
     this.taskService.getAllTasks().subscribe(tasks => {
       this.tasks=tasks;
-     // const recibir=sessionStorage.getItem("id");//setItem("id", dataResponse.nombre_supermercado);
       this.sup_recibido = String(sessionStorage.getItem("id"));
       let firstName = sessionStorage.getItem("id");
       console.log(`Hola, mi nombre es ${firstName}`)
-     // console.log('recibir',sessionStorage.getItem("id"));
       console.log('recibir',firstName);
-     // console.log('Tareas',this.tasks);
       Object.keys(this.tasks).forEach(key => {
         if (String(this.tasks[key].Supermercado_idSupermercado) == firstName) {    
             this.supermercado.push({
               ...this.tasks[key]
             });
         }
-       
     });
     console.log("Listado de los supermercado que son iguales a 3",this.supermercado);
-    
-      /*this.tasks.forEach((task: any) => {
-        console.log(task);
-      const findSup = task.find((obj: { Supermercado_idSupermercado: string; }) => obj.Supermercado_idSupermercado == '3');
-      if (findSup) {
-      this.supermercado.push({
-        ...task
-      });
-    }
-  });*/
     });
   }
   addProduct(idProducto: any,index: any){
       console.log([idProducto,index]);
       let cantidad:number = parseInt((<HTMLInputElement>document.getElementById(`cantidad${index}`)).value);
-     // let cantidad:any = Object.keys(this.tasks).find(x => this.tasks[x]. == idProducto);
       let indproducto:any = Object.keys(this.tasks).find(x => this.tasks[x].idProducto == idProducto);
       let subtotal=cantidad*this.tasks[indproducto].precio;
       console.log([cantidad]);
@@ -96,8 +59,7 @@ export class ProductoComponent {
             "nombre":this.tasks[indproducto].nombre_producto,
             "unidad":this.tasks[indproducto].unidad,
             "precio":this.tasks[indproducto].precio,
-            "cantidad":cantidad,
-            //"cantidad":this.tasks[indproducto].stock,     
+            "cantidad":cantidad,  
             "subtotal":subtotal
           }); 
           this.ftotal();
@@ -109,7 +71,6 @@ export class ProductoComponent {
           alert("Error al ingresar los productos");
         console.log('nos van a despedir');
         }
-
       }
   }  
   remProduct(id:any){
@@ -123,27 +84,14 @@ export class ProductoComponent {
         this.totalfinal += pro.subtotal;
   }); 
   }
-  //pvp=
   ngOnInit(): void{
   }
-
-  //mensajeIngreso: any = "";
   enviarprecio() {
     console.log("el total final", this.totalfinal);
         let idS: string = "" + this.totalfinal;
         sessionStorage.setItem("tpvp", idS);
         console.log("convertido a string es: " + idS)
         this.router.navigate(['supermercados']);
-      /*else {
-        this.mensajeIngreso = "NO SE PUEDE INGRESAR A LOS PRODUCTOS";
-      }*/
     }
-
-  
-  /*openDialog() {    
-     const dialogConfig = new MatDialogConfig();          
-      dialogConfig.disableClose = false;     
-      dialogConfig.autoFocus = true;     
-     const dialogRef = this.dialog.open(PagoComponent, dialogConfig);     }*/
 }
 
