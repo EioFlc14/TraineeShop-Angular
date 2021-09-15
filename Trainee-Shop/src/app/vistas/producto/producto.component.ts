f44f475f3a4dc78f30715af2bf8199a5da5987b2
AlexanderJr1996 <alexander_navarrete2011@hotmail.com>
2021-09-15 18:08:18 -0500

    validado el stock

--- Trainee-Shop/src/app/vistas/producto/producto.component.ts
+++ Trainee-Shop/src/app/vistas/producto/producto.component.ts
@@ -16,6 +16,7 @@ export class ProductoComponent {
   pvp:number=0;
   acu:number=0;
   res:number=0;
+  res1:number=0;
   sup_recibido:string="";
   confirmar: boolean=true;
   constructor(
@@ -41,24 +42,26 @@ export class ProductoComponent {
     });
   }
   addProduct(idProducto: any,index: any){
+      this.res=0;
+      this.res1=0;
       console.log([idProducto,index]);
       let cantidad:number = parseInt((<HTMLInputElement>document.getElementById(`cantidad${index}`)).value);
       let indproducto:any = Object.keys(this.tasks).find(x => this.tasks[x].idProducto == idProducto);
+      let prostock:any = this.total.filter(x => x.id == idProducto);
+      console.log("esto tiene el prostock",prostock);
       let subtotal=cantidad*this.tasks[indproducto].precio;
       console.log([cantidad]);
       console.log(this.tasks[indproducto]);
       console.log(typeof this.tasks);
+      prostock.forEach((z:any) => {
+        this.res1=this.res1+z.cantidad;
+      });
+      this.res=parseInt(this.tasks[indproducto].stock)-this.res1-cantidad;
+      console.log("esto tiene el res final",this.res );
       if(this.tasks[indproducto].stock){
-        if((cantidad>0) && (cantidad <= parseInt(this.tasks[indproducto].stock)) ){
-          this.res=parseInt(this.tasks[indproducto].stock);
+        if((cantidad>0) && (this.res <= parseInt(this.tasks[indproducto].stock)) && (this.res>=0) ){
           console.log('esto tiene el res',this.res);
-          this.res=this.res-cantidad;
           console.log('esto tiene el res 2',this.res);
-        //cantidad=this.res; 
-          if(this.res==0){
-            alert("No existe el stock solicitado !!!");
-          }
-         else{
           this.total.push({
             "id":this.tasks[indproducto].idProducto,
             "nombre":this.tasks[indproducto].nombre_producto,
@@ -68,7 +71,6 @@ export class ProductoComponent {
             "subtotal":subtotal
           }); 
           this.ftotal();
-         }
         }else{
           alert("Error al ingresar los productos !!!");
         }
