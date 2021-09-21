3fbc46e5a528ef8257d92c957596d079e53c1889
Edison <edison14_@hotmail.com>
2021-09-20 23:06:02 -0500

    Nuevas Ips

--- Trainee-Shop/src/app/servicios/api/api-pagos.service.ts
+++ Trainee-Shop/src/app/servicios/api/api-pagos.service.ts
@@ -9,9 +9,9 @@ import {Observable} from 'rxjs'
 export class ApiPagosServicio {
 
 
-  url:string = "http://3.135.239.12:5000/users?token="+"000"+"-";
-  url2:string = "http://3.135.239.12:5000/pays?token="+"000"+"-";
-
+  url:string = "http://18.220.62.211:5000/users?token="+"000"+"-";
+  url2:string = "http://18.220.62.211:5000/pays?token="+"000"+"-";
+  
   constructor(
     private _http:HttpClient
     ) { }
